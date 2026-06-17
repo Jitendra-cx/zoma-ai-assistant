8b8249b24583b5ef306fcf1b616b8955766720b3
cx-jitendra <jitendra.kumar@codelogicx.com>
2026-06-17 19:52:00 +0530

    modified

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // test.js
 // Classic security flaw: Hardcoded API keys or credentials
 const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE/SECRETKEY/SHOULDNOTBEHERE";
-const github_token = "ghp_vulnerableTokenValue1234567890abcdefgh";
+const github_token = "ghp_vulnerableTokenValue1234567890abcdefghg";
 
 console.log("Initializing service with key...");
 
