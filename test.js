97b04f25d64796b10336ee03d2768e8174e15a3e
cx-jitendra <jitendra.kumar@codelogicx.com>
2026-06-21 20:45:48 +0530

    modified

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ app.get('/run', (req, res) => {
     // 🚨 CRITICAL SECURITY FLAW: Remote Code Execution (RCE) via eval()
     eval(userInput); 
 
-console.lo('foo abcdeFG')
+console.lo('foo A')
 
     res.send('Executed!');
 });
