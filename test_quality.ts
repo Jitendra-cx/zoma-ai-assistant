f221e256fff186679935bca35ad80bafd277eef9
cx-jitendra <jitendra.kumar@codelogicx.com>
2026-06-24 17:27:30 +0530

    test ai review

--- test_quality.ts
+++ test_quality.ts
@@ -1,7 +1,7 @@
-function processUserData(inputString: any) {
-    // Triggers ESLint & Semgrep
-    return eval(inputString);
-}
+// function processUserData(inputString: any) {
+//     // Triggers ESLint & Semgrep
+//     return eval(inputString);
+// }
 
 // Triggers TypeScript Compiler (tsc)
 let configurationPort: number = "8080"; 
