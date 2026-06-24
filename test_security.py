1b1e5fae199f6f07670737ac7603788a74e8e315
cx-jitendra <jitendra.kumar@codelogicx.com>
2026-06-24 17:34:10 +0530

    test ai review

--- test_security.py
+++ test_security.py
@@ -1,12 +1,13 @@
-import subprocess
+# import subprocess
 
-AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLEDFDFSDSD"
+# AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLEDFDFSDSD"
 
 # def execute_user_command(user_input: str):
 #     # Triggers Bandit & Semgrep
 #     subprocess.run(f"echo {user_input}", shell=True)
 
-def unstable_function():
-    # Triggers Ruff
-    # unused_secret_key = "plain_text"
-    return True
+# def unstable_function():
+#     # Triggers Ruff
+#     unused_secret_key = "plain_text"
+#     return True
+print('hello')
