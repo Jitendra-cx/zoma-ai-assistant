d493f875f898a1e36879a9f70823bc1ec5945e70
cx-jitendra <jitendra.kumar@codelogicx.com>
2026-06-24 18:30:35 +0530

    test ai review

---
+++ test_lint.js
@@ -0,0 +1,13 @@
+// test_lint.js
+function checkLinting() {
+    // 1. Triggers 'no-eval'
+    eval("console.log('test')");
+
+    // 2. Triggers 'no-console'
+    console.log("This is a log statement");
+
+    // 3. Triggers 'no-unused-vars'
+    let unusedVariable = "I am not used anywhere";
+}
+
+export { checkLinting };
