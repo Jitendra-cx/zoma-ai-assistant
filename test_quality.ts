747591221f09e03ed683b6591224c70ea4779182
cx-jitendra <jitendra.kumar@codelogicx.com>
2026-06-24 14:28:25 +0530

    undo change

--- test_quality.ts
+++
@@ -1,9 +0,0 @@
-function processUserData(inputString: any) {
-    // Triggers ESLint & Semgrep
-    return eval(inputString);
-}
-
-// Triggers TypeScript Compiler (tsc)
-let configurationPort: number = "8080"; 
-
-export { processUserData, configurationPort };
